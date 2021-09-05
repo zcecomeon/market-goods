5443459b07482b5a6b44b3c955c0a4aa9263cc0a
zcecomeon <2456719711@qq.com>
2021-09-05 18:14:51 +0800

    修改typecontroller

--- src/main/java/com/zce/market/controller/pages/back/type/TypeController.java
+++ src/main/java/com/zce/market/controller/pages/back/type/TypeController.java
@@ -1,44 +1,41 @@
 package com.zce.market.controller.pages.back.type;
 import com.zce.market.controller.BaseController;
+import com.zce.market.pojo.dto.ResponseDTO;
 import com.zce.market.pojo.entity.Goods;
+import com.zce.market.pojo.entity.GoodsType;
 import com.zce.market.service.GoodsService;
+import com.zce.market.service.TypeService;
+import org.apache.coyote.Response;
 import org.mybatis.logging.Logger;
 import org.mybatis.logging.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.annotation.Resource;
 import java.util.Map;
-
+/*
+*@author: 时光荒凉了来时路
+*@date: 
+*@param: 
+*@return: 
+*@description: Read the notes carefully
+**/
 @Controller
-@RequestMapping("/pages/back/goods")
+@RequestMapping("/pages/back/type")
 public class TypeController extends BaseController {
-
     Logger logger = LoggerFactory.getLogger(TypeController.class);
-
     @Resource
-    GoodsService goodsService;
-
+    TypeService typeService;
     @RequestMapping("addPre")
-    String addPre(){
-        return "pages/back/goods/goods-addPre";
+    String addPre(Model model){
+        return "pages/back/type/type-addPre";
     }
-
     @RequestMapping("add")
     @ResponseBody
-    Map<String,Object> add(Goods goods, MultipartFile pic){
-        System.err.println(goods);
-        //视频代码
-        if("on".equals(goods.getOnSale())){
-            goods.setOnSale("Yes");
-        }else {
-            goods.setOnSale("NO");
-        }
-        String imgUrl = uploadFile("/upload/goods/", pic);//图片存放数据库路径
-        goods.setImg(imgUrl);
-        return goodsService.add(goods);
+    ResponseDTO add(GoodsType goodsType){
+        return typeService.add(goodsType);
     }
 
 }
